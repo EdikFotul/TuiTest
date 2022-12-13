680fdc9e6d9070affda9ffa7b2ef0c5e3fcc6f23
Eduard Fotul <edf@ciklum.com>
2022-12-13 13:34:33 +0200

    Initial commit

---
+++ src/main/java/com/test/mobile/tui/TuiTest/TuiTestApplication.java
@@ -0,0 +1,13 @@
+package com.test.mobile.tui.TuiTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuiTestApplication.class, args);
+	}
+
+}
